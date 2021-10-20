0459691d460674ebf159d38f0856e378bd3fa2a1
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-10-19 22:56:29 -0400

    testing

--- test/main.py
+++ test/main.py
@@ -1,6 +1,8 @@
 import lwforecast as lwf
 import lwforecast as lwf
 
-data = lwf.parse_csv("training.csv", 0)
+data = lwf.parse_csv("training.csv", 1)
 model = lwf.train(data)
+
 print(lwf.calc_acc(data, model))
+print(lwf.predict([480, 420, 3866, 6, 8], model))
